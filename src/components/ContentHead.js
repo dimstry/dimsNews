45e920bd44554266b782b8d74e3ff211c83a6aa5
dimstry <dimastriana03@gmail.com>
2022-08-16 15:22:09 +0700

    finish content head

--- src/components/ContentHead.js
+++ src/components/ContentHead.js
@@ -1,21 +1,87 @@
+import {
+    Card
+} from "react-daisyui";
+
+import "../App.css";
+
 
 function ContentHead() {
     return(
         <div className="flex flex-row w-full gap-2">
-            <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-                content
+            <div className="grid flex-grow h-auto card place-items-center">
+                <Card style={{ width: '100%', cursor: 'pointer'}} >
+                    <div className="card">
+                    <Card.Image
+                        src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Shoes"
+                        className="card_image"
+                    />
+                    </div>
+                    <Card.Body className="card_body">
+                        <Card.Title tag="h2">Shoes!</Card.Title>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </Card.Body>
+                </Card>
             </div>
-            <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-                content
+            <div className="grid flex-grow h-auto card place-items-center">
+                <Card style={{ width: '100%', cursor: 'pointer'}} >
+                    <div className="card">
+                    <Card.Image
+                        src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Shoes"
+                        className="card_image"
+                    />
+                    </div>
+                    <Card.Body className="card_body">
+                        <Card.Title tag="h2">Shoes!</Card.Title>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </Card.Body>
+                </Card>
             </div>
-            <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-                content
+            <div className="grid flex-grow h-auto card place-items-center">
+                <Card style={{ width: '100%', cursor: 'pointer'}} >
+                    <div className="card">
+                    <Card.Image
+                        src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Shoes"
+                        className="card_image"
+                    />
+                    </div>
+                    <Card.Body className="card_body">
+                        <Card.Title tag="h2">Shoes!</Card.Title>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </Card.Body>
+                </Card>
             </div>
-            <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-                content
+            <div className="grid flex-grow h-auto card place-items-center">
+                <Card style={{ width: '100%', cursor: 'pointer'}} >
+                    <div className="card">
+                    <Card.Image
+                        src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Shoes"
+                        className="card_image"
+                    />
+                    </div>
+                    <Card.Body className="card_body">
+                        <Card.Title tag="h2">Shoes!</Card.Title>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </Card.Body>
+                </Card>
             </div>
-            <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-                content
+            <div className="grid flex-grow h-auto card place-items-center">
+                <Card style={{ width: '100%', cursor: 'pointer'}} >
+                    <div className="card">
+                    <Card.Image
+                        src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="Shoes"
+                        className="card_image"
+                    />
+                    </div>
+                    <Card.Body className="card_body">
+                        <Card.Title tag="h2">Shoes!</Card.Title>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     )
