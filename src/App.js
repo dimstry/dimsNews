c0216db94be3013f2039164517d75b8f821029c5
dimstry <dimastriana03@gmail.com>
2022-08-18 14:08:14 +0700

    fech api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ContentHead from './components/ContentHead';
+import { data } from "autoprefixer";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  // init
+  const [datas, setDatas] = useState([])
+
+  // get data dari api
+  const getDatas = () => { 
+      axios.get('https://newsapi.org/v2/top-headlines', {
+          params: {
+              country: 'id',
+              apiKey: 'da3148f0a64b42e4b43d6fa816a0d3f8',
+          }
+      })
+      .then(res => {
+        //set data
+          const value = res.data
+          setDatas(value)
+      })
+      .catch(err => console.warn(err));
+  }
+  useEffect(() => {
+      getDatas();
+  }, [])
+
   return (
     <div className="App">
       <NavBar />
       <div className='container mx-auto px-5 lg:px-40 mt-10'>
         <h1 className='text-left text-3xl my-4'>News 🔥</h1>
-        <ContentHead />
+        <ContentHead {...datas}/>
+      </div>
+      <div className='h-48 bg-slate-400  container mx-auto px-5 lg:px-40 flex justify-center'>
+        <div className='w-64 main_left bg-slate-50'>
+          <h2>Left</h2>
+        </div>
+        <div className='main_right'>
+          <h2>Right</h2>
+        </div>
       </div>
     </div>
   );
--- src/components/ContentHead.js
+++ src/components/ContentHead.js
@@ -2,90 +2,34 @@ import {
     Card,
     Carousel
 } from "react-daisyui";
-
 import "../App.css";
 
 
-function ContentHead() {
+function ContentHead(datas) {
+
     return(
-        <div className="md:flex gap-3">
-            <div className="md:flex-1">
-                <Card style={{ width: '100%', cursor: 'pointer'}} >
-                    <div className="card">
-                        <Card.Image
-                            src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                            alt="Shoes"
-                            className="card_image"
-                        />
+        <div className="gap-3 md:flex">
+            {datas.articles ? datas.articles.slice(0, 5).map((data, i) => {
+                return(
+                    <div className="md:flex-1 mb-4" key={i}>
+                        <a href={data.url}>
+                            <Card style={{ width: '100%', cursor: 'pointer'}} >
+                                <div className="card">
+                                    <Card.Image
+                                        src={data.urlToImage}
+                                        alt="Shoes"
+                                        className="card_image"
+                                    />
+                                </div>
+                                <Card.Body className="card_body m-1">
+                                    <Card.Title tag="p">{data.title}</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </a>
                     </div>
-                    <Card.Body className="card_body">
-                        <Card.Title tag="h2">Shoes!</Card.Title>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                    </Card.Body>
-                </Card>
-            </div>
-            <div className="contents">
-                <div className="md:flex-1">
-                    <Card style={{ width: '100%', cursor: 'pointer'}} >
-                        <div className="card">
-                            <Card.Image
-                                src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                                alt="Shoes"
-                                className="card_image"
-                            />
-                        </div>
-                        <Card.Body className="card_body">
-                            <Card.Title tag="h2">Shoes!</Card.Title>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="md:flex-1">
-                    <Card style={{ width: '100%', cursor: 'pointer'}} >
-                        <div className="card">
-                            <Card.Image
-                                src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                                alt="Shoes"
-                                className="card_image"
-                            />
-                        </div>
-                        <Card.Body className="card_body">
-                            <Card.Title tag="h2">Shoes!</Card.Title>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="md:flex-1">
-                    <Card style={{ width: '100%', cursor: 'pointer'}} >
-                        <div className="card">
-                            <Card.Image
-                                src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                                alt="Shoes"
-                                className="card_image"
-                            />
-                        </div>
-                        <Card.Body className="card_body">
-                            <Card.Title tag="h2">Shoes!</Card.Title>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </div>
-            <div className="md:flex-1">
-                <Card style={{ width: '100%', cursor: 'pointer'}} >
-                    <div className="card">
-                        <Card.Image
-                            src="https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                            alt="Shoes"
-                            className="card_image"
-                        />
-                    </div>
-                    <Card.Body className="card_body">
-                        <Card.Title tag="h2">Shoes!</Card.Title>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                    </Card.Body>
-                </Card>
-            </div>
+                )
+            }): ""
+            }
         </div>
         
     )
